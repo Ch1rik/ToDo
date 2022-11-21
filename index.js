9155aeea78b8ac759ca69da1dd545ab31bc7b5a5
Ch1rik <sluchakirill@gmail.com>
2022-11-21 19:55:25 +0300

    implementedtodolist

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-const button = document.getElementById("button")
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+const space = /\s*/;
 let amount = 1;
-button.addEventListener("click", function () {
-    const input = document.getElementById("input");
-    const li = document.createElement('li')
-    const ul = document.getElementById("ul")
-    li.innerText = input.value;
-    input.value="";
-    ul.append(li)
-    amount++
-    if (amount % 2) {
-        ul.classList.remove("block")
-        ul.classList.add("list")
+function launcher() {
+    input.value = input.value.replace(space, '');
+    if (input.value) {
+        input.classList.remove("error");
+        const li = document.createElement('li');
+        li.innerText = input.value;
+        input.value = "";
+        ul.append(li);
+        amount++;
+        if (amount % 2) {
+            ul.classList.remove("block");
+            ul.classList.add("list");
+        } else {
+            ul.classList.remove("list");
+            ul.classList.add("block");
+        }
     } else {
-        ul.classList.remove("list")
-        ul.classList.add("block")
+        input.classList.add("error");
+        alert("fill in the field");
     }
-})
+}
+button.addEventListener("click", function () {
+    launcher();
+    input.focus();
+});
+input.addEventListener('keydown', function (event) {
+    if (event.code === 'Enter') {
+        launcher();
+    }
+});
+
+
+
